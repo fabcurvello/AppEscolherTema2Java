8e469c7b9ae7506b2e2d4b6c028cef0d9169abbd
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-09-03 21:20:34 -0300

    App completo

--- app/src/main/java/br/com/fabriciocurvello/appescolhertema2java/MainActivity.java
+++ app/src/main/java/br/com/fabriciocurvello/appescolhertema2java/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
